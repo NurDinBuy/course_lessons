805f9540457ee5f0db28ea41f65b34ce011b3970
NurDinBuy <nurik161103@gmail.com>
2022-11-12 19:07:12 +0600

    lesson4

---
+++ 20-03/lesson4_20-03/main.js
@@ -0,0 +1,86 @@
+// console.log(document)
+// console.dir(document)
+
+// window.alert('HI')
+// window.confirm('Hello')
+
+// document.body.style.background = 'red'
+
+// const button = document.getElementById('btn')
+
+// const hello = document.getElementById('hello')
+// hello.textContent = 'Hello! From JS'
+// console.log(hello.textContent )
+
+
+// const button = document.querySelector('#btn')
+
+// button.addEventListener('click', (event) => {
+//     console.log(event)
+// })
+
+// button.addEventListener('click', function () {
+//     alert('Hello from button click')
+// })
+
+// const posX = document.getElementById('posX') // только 1 элемент
+// const posY = document.getElementById('posY')
+
+// window.addEventListener('mousemove', event => {
+//     let x = event.screenX
+//     let y = event.screenY
+//     posX.innerHTML = x
+//     posY.innerHTML = y
+// })
+
+// const div = document.getElementsByClassName('div') // только 1 элемент
+// const div = document.querySelector('.div') // только 1 элемент
+// const colors = document.querySelectorAll('.div') // много элементов
+
+// for (let item of colors) {
+    // item.onclick = event => document.body.style.backgroundColor = event.target.innerText
+    // item.onclick = (e) => console.log(e)
+// }
+
+// filter()
+
+// const fruits = ['apple', 'pineapple', 'peach', 'grape']
+//
+// const result = fruits.filter(fruit => fruit.length === 5)
+//
+// console.log(fruits)
+// console.log(result)
+
+// map()
+
+// const numbers = [2, 34, 56, 89]
+//
+// const newNumbers = numbers.map(num => num * 2)
+//
+// console.log(numbers)
+// console.log(newNumbers)
+
+
+// forEach()
+// const numbers = [2, 34, 56, 89, 'dfg', 19, false]
+//
+// let count = 0
+//
+// numbers.forEach(element => {
+//     console.log(element)
+//     count++
+// })
+//
+// console.log(`Количество элементов: ${count}`)
+
+// slice()
+
+// const animals = ['ant', 'bison', 'camel', 'duck', 'elephant']
+
+// let slicedAnimals = animals.slice(2)
+
+// console.log(animals.slice(-1))
+
+// console.log(animals)
+// console.log(slicedAnimals)
+
