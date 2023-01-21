844731d7102e597d939d54ac8ab548ee772075cd
NurDinBuy <94064993+NurDinBuy@users.noreply.github.com>
2023-01-21 20:10:32 +0600

    lesson4

---
+++ 21-03/lesson4/main.js
@@ -0,0 +1,48 @@
+// console.log('HI')
+// const obj = {
+//     name: 'Nurdin',
+//     age: 19
+// }
+// console.log(obj);
+// const data = JSON.stringify(obj)
+// console.log(data)
+// const obj1 = JSON.parse(data)
+// console.log(obj.name)
+
+// const btn = document.querySelector('button')
+
+// btn.addEventListener('click', () => {
+//     const request = new XMLHttpRequest() // создание запроса
+//     request.open("GET", "data.json") // объявление метода запроса и указание пути
+//     request.setRequestHeader("Content-type", "application/json") // указывание заголовка
+//     request.send() // отправка запроса
+//     request.addEventListener('load', () => {
+//         // console.log(JSON.parse(request.response))
+//         const data = JSON.parse(request.response)
+//         document.querySelector('.name').innerHTML = data.name
+//         document.querySelector('.age').innerHTML = data.age
+//     })
+// })
+
+// console.log(2 / 'two')
+// str num bool undefined null NaN - primitive
+// obj arr - obj [ссылочными]
+
+// let a = 5
+// let b = a + 15
+//
+// console.log(a)
+// console.log(b)
+
+
+// const obj = {
+//     name: 'Hanna',
+//     age: 21
+// }
+//
+// const obj2 = { ...obj }
+// obj2.name = 'Rose'
+//
+// console.log(obj)
+// console.log(obj2)
+
