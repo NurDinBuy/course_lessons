ddf0e5b19b34ab36d7e3f30c931c3ad925883dd7
NurDinBuy <94064993+NurDinBuy@users.noreply.github.com>
2023-02-01 20:09:41 +0600

    lesson1

---
+++ 23-01/lesson1/main.js
@@ -0,0 +1,83 @@
+// variable - переменная
+
+// Примитивные типы данных (6)
+// var name = 'Nurdin' // string
+// var number = 123 // number
+// var boolean1 = false // boolean
+// var boolean2 = true // boolean
+// var neopr // undefined
+// var nichego = null // null
+// var notNumber = 12 - 'два' // NaN
+
+// typeof - оператор для вычисления типов данных у переменных
+
+// Не примитивные типы данных (2) ссылочные
+
+// Array - Массив
+
+// var arr = [1, 2, 3, 4, 5]
+
+// Object - Объект
+
+// var obj = {
+//     name: 'Nurdin',
+//     age: 19
+// }
+
+// Операторы (Математические / Условные / Логические )
+
+// console.log(12 + 30)
+// console.log(54 - 12)
+// console.log(40 / 2)
+// console.log(2 * 3)
+
+
+// console.log(12 < 100)
+// console.log(12 > 100)
+// console.log(10 >= 11)
+// console.log(10 <= 11)
+// console.log(100 === 100)
+// console.log(100 !== 100)
+
+
+
+// Условные конструкции if, else if, else
+
+
+// if (false) {
+//     console.log('Yes')
+// }
+
+// var name = prompt('Введи имя')
+// document.write(name)
+
+// var age = Number(prompt('Введите ваш возраст'))
+//
+// if (age >= 18 && age < 50) {
+//     alert('Вход разрешен')
+// } else if (age < 18) {
+//     alert('Вход запрещен!')
+// } else if (age > 50) {
+//     alert('Ты стар для этого')
+// } else {
+//     alert('Вводи возраст а не буквы')
+// }
+
+// Условные конструкция switch case
+
+// var colors = prompt('Выбери цвет: (красный, желтый, зеленый)').toLowerCase().trim()
+//
+// switch (colors) {
+//     case 'красный':
+//         alert('СТОП!')
+//         break
+//     case 'желтый':
+//         alert('ЖДИ!')
+//         break
+//     case 'зеленый':
+//         alert('ИДИ!')
+//         break
+//     default:
+//         alert('Выбирай цвета!')
+//         break
+// }
