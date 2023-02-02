e08c3db00fbdbbc4905b42d1da9a5d1820525c46
NurDinBuy <94064993+NurDinBuy@users.noreply.github.com>
2023-02-02 15:21:35 +0600

    lesson9

---
+++ 22-01/lesson9/main.js
@@ -0,0 +1,20 @@
+// let audio = new Audio()
+// audio.preload = 'auto'
+// audio.src = 'patron.mp3'
+
+// console.dir(audio)
+
+// const play = () => {
+//     audio.play()
+// }
+//
+// const stop = () => {
+//     audio.pause()
+// }
+
+// const video = document.querySelector('video')
+// video.controls = true
+// console.log(video)
+// const video = new HTMLVideoElement()
+//
+// console.dir(video)
