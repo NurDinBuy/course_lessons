25f82a812fc4f5637dd2a15d6e4665dd96086c3d
NurDinBuy <94064993+NurDinBuy@users.noreply.github.com>
2023-02-15 20:08:43 +0600

    lesson3

---
+++ 22-03/lesson3/examples/main.js
@@ -0,0 +1,34 @@
+const btns = document.querySelectorAll('button')
+const wrapper = document.querySelector('.btn-block')
+
+// console.log(btns[0])
+// btns[0].style.background = 'red'
+
+// btns[0].classList.add('red')
+// btns[0].classList.remove('red')
+
+// btns.forEach(btn => {
+//    btn.addEventListener('click', (event) => {
+//         if (event.target.classList.contains('red')) {
+//             event.target.classList.remove('red')
+//         } else {
+//             event.target.classList.add('red')
+//         }
+//    })
+// })
+//
+
+// wrapper.onclick = (event) => {
+//     if (event.target.tagName === 'BUTTON') {
+//         event.target.onclick = (event) => {
+//             if (event.target.classList.contains('red')) {
+//                 event.target.classList.remove('red')
+//             } else {
+//                 event.target.classList.add('red')
+//             }
+//         }
+//     }
+// }
+//
+// const newButton = document.createElement('button')
+// wrapper.append(newButton)
