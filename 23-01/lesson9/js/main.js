5858c2c2a1ac8a3a120b5b3d5257dc8d709f94fb
NurDinBuy <94064993+NurDinBuy@users.noreply.github.com>
2023-02-28 17:45:16 +0600

    lesson 9

---
+++ 23-01/lesson9/js/main.js
@@ -0,0 +1,26 @@
+// function insert (num) {
+//     document.form.textview.value = document.form.textview.value + num;
+// }
+// function clean () {
+//     document.form.textview.value = ""
+// }
+// function back () {
+//     let exp = document.form.textview.value;
+//     document.form.textview.value = exp.substring(0 , exp.length-1);
+// }
+// function equal () {
+//     let exp = document.form.textview.value;
+//     if (exp) {
+//         document.form.textview.value = eval(exp);
+//     }
+// }
+
+
+// let str = 'Nurdin'
+// for (let i = 0; i < str.length; i++) {
+//     console.log(str[i])
+// }
+
+
+const rand = () => console.log(Math.floor(Math.random() * 100 - 100))
+
