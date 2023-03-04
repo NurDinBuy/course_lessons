8c30b8bc669716f2cd7be5c4de06055bb11b24da
NurDinBuy <94064993+NurDinBuy@users.noreply.github.com>
2023-03-04 20:06:40 +0600

    lesson 1

---
+++ 24-01/lesson1/main.js
@@ -0,0 +1,105 @@
+// console.log('My name is Nurdin')
+
+
+
+// Переменные - variable
+
+// var name = 'Nurdin'
+// console.log(name)
+
+// Типы данных - всего 8 штук но 2 типа данных не признаются
+// Примитивные типы данных
+var string = 'Hello' // string
+// var number = 123456 // number
+//
+// var boolean1 = false // boolean
+// var boolean2 = true // boolean
+//
+// var neorp = undefined // undefined
+// var nichego = null // null
+// var nan = 12 - 'два' // NaN - not a number
+
+// typeof - оператор для вычисления типов данных у переменных
+// console.log(typeof nan)
+
+// Не примитивные типы данных (2) ссылочные
+
+// Array - Массив
+
+// var arr = [12, 'str', null, [string], false]
+// console.log(arr[3][0])
+
+ // var arr2 = [1, 2, 3, 4, 5]
+// console.log(arr2)
+
+// Object - Объект
+
+// var obj = {
+//     name: 'Nurdin',
+//     age: 19,
+//     arr: [1, 2, 3, 4 ],
+//     arr2: arr2,
+//     boolean: false,
+//     obj2: {
+//         name: 'Bek',
+//         age: 20,
+//     }
+// }
+
+// Операторы (Математические / Условные / Логические )
+
+// console.log(20 + 2)
+// console.log(20 - 2)
+// console.log(20 * 2)
+// console.log(20 / 2)
+// console.log(2 ** 3)
+
+// console.log(12 < 100)
+// console.log(12 > 100)
+// console.log(10 >= 11)
+// console.log(10 <= 11)
+// console.log(100 == '100')
+// console.log(100 === '100')
+// console.log(100 !== 99)
+// console.log(!!99)
+// var yes = false
+// console.log(yes)
+
+
+// Условные конструкции (if, else if, else)
+
+// if (100 === 99) {
+//     console.log('Hello')
+// }
+
+// window.alert('Hello')
+// console.log(console)
+// console.log(window)
+
+// var name = prompt('введите имя')
+//
+// console.log(name)
+// alert(name)
+
+// var str = '123'
+// console.log(typeof Number(str))
+
+var age = Number(prompt('Введите ваш возраст!'))
+
+// && - Логическое 'И'
+// || - Логическое 'Или'
+
+// if (age >= 18 && age < 50) {
+//     alert('Вход разрешён!')
+// } else if (age < 18) {
+//     alert('Вход запрещён!')
+// } else if (age > 50) {
+//     alert('Ты стар для этого')
+// } else {
+//     alert('Вводи возраст, а не буквы!')
+// }
+
+
+// Условные конструкция switch case
+
+
